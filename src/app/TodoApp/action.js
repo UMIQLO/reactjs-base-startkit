7b9829cf18f0504c13dd799fc125654c39f4b863
UMIQLO <umiqlo.moe@gmail.com>
2018-04-03 00:22:56 +0800

    using react-redux to create TodoApp

---
+++ src/app/TodoApp/action.js
@@ -0,0 +1,12 @@
+// @flow
+export const onItemAdd = (payload: { id: number, text: string }) => (
+  {
+    type: 'ADD_ITEM',
+    payload
+  }
+)
+
+export const onItemDel = (id: number) => ({
+  type: 'DEL_ITEM',
+  id
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import itemApp from './app/TodoApp/reducer'
+import MyComponent from './app/TodoApp/app'
 import HelloReact from './app/HelloWorld/HelloReact'
 
+const store = createStore(
+  itemApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
+
 ReactDOM.render(
-  <HelloReact />,
+  <div>
+    <HelloReact />
+    <Provider store={store}>
+      <MyComponent />
+    </Provider>
+  </div>,
   document.getElementById('root'),
 )
